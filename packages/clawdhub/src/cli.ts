61e953d17cd1d9c873f4f11b031669e82ed57262
Deonte Cooper <DJC00P@MacBook-Pro.local>
2026-03-21 01:01:09 -0600

    Wired up the list-stars command

--- packages/clawdhub/src/cli.ts
+++ packages/clawdhub/src/cli.ts
@@ -24,6 +24,7 @@ import {
   cmdUpdate,
 } from "./cli/commands/skills.js";
 import { cmdStarSkill } from "./cli/commands/star.js";
+import { cmdListStars } from "./cli/commands/listStars.js";
 import { cmdSync } from "./cli/commands/sync.js";
 import {
   cmdTransferAccept,
@@ -445,6 +446,14 @@ program
   .action(async (slug, options) => {
     const opts = await resolveGlobalOpts();
     await cmdUnstarSkill(opts, slug, options, isInputAllowed());
+  });
+
+program
+  .command("list-stars")
+  .description("List your starred skills")
+  .action(async () => {
+    const opts = await resolveGlobalOpts();
+    await cmdListStars(opts);
   });
 
 program
